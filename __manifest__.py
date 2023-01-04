458ca68f7d38bccb77aff9daf10e2529d20b0482
Ahmed <ahmed.abdelbakey.18@gmail.com>
2023-01-04 21:45:04 +0300

    [ADD] add link to offer_view

--- __manifest__.py
+++ __manifest__.py
@@ -3,12 +3,14 @@
     'author': 'Ahmed Abdelbaky',
     'description': """odoo15 documentation Project """,
     'data': [
-        #'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menu.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_type_menuitem.xml',
         'views/estate_tage_view.xml',
         'views/estate_tage_menuitem.xml',
+        'views/estate_offer_view.xml',
+
     ],
 }
