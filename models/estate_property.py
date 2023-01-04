38998475a7486431643481fd3bba008ebaa09f17
Ahmed <ahmed.abdelbakey.18@gmail.com>
2023-01-04 12:14:45 +0300

    [ADD] add new relation many to one fields

--- models/estate_property.py
+++ models/estate_property.py
@@ -37,3 +37,6 @@ class estateProperty(models.Model):
         ],
         default='new', required=True, copy=False, string='Status'
         )
+    type_id = fields.Many2one('estate.property.type', string='Property Type')
+    sales_person = fields.Char('Sales Person', default=lambda self: self.env.user.name, readonly=True)
+    partner_id = fields.Many2one('res.partner', string='Buyer')
