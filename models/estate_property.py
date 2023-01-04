152de56ffeb66d722e602a6c96b92f8b05fdaf79
Ahmed <ahmed.abdelbakey.18@gmail.com>
2023-01-04 21:41:03 +0300

    [ADD] add new one2many relation 'offer_ids'

--- models/estate_property.py
+++ models/estate_property.py
@@ -41,3 +41,4 @@ class estateProperty(models.Model):
     sales_person = fields.Char('Sales Person', default=lambda self: self.env.user.name, readonly=True)
     partner_id = fields.Many2one('res.partner', string='Buyer')
     tag_ids = fields.Many2many('estate.property.tag', string='Tag')
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
