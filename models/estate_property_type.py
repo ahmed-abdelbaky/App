ee35b8f255750e77a4d899a2b301976ef8e7023a
Ahmed <ahmed.abdelbakey.18@gmail.com>
2023-01-04 12:43:30 +0300

    [REF] refactor name of field

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 class estatePropertyType(models.Model):
     _name = "estate.property.type"
 
-    name = fields.Char('Property Type', required=True)
+    name = fields.Char('Name', required=True)
