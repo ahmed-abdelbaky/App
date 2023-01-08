356301a8d48bf43055d20c9cf29d62081cde6647
Ahmed <ahmed.abdelbakey.18@gmail.com>
2023-01-08 20:06:52 +0300

    [ADD] add compute and inverse field

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 
 class estatePropertyOffer(models.Model):
@@ -13,3 +15,16 @@ class estatePropertyOffer(models.Model):
       )
     partner_id = fields.Many2one("res.partner", string="Partner")
     property_id = fields.Many2one("estate.property", string="Property")
+    validity = fields.Integer("Validity", default='7')
+    date_deadline = fields.Date('Date Deadline', compute="_compute_date", inverse='_inverse_date')
+
+    @api.depends('validity')
+    def _compute_date(self):
+        for record in self:
+            record.date_deadline = self.env.user.create_date + relativedelta(days=record.validity)
+            print('record.date_deadline', record.date_deadline)
+
+
+    def _inverse_date(self):
+        for record in self:
+            record.date_deadline = record.datetime()
