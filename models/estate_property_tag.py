8a49636af7aefb8416af1476497ed0c2feec3eb7
Ahmed <ahmed.abdelbakey.18@gmail.com>
2023-01-10 19:17:16 +0300

    [ADD] add sql constrain on tag name

--- models/estate_property_tag.py
+++ models/estate_property_tag.py
@@ -5,3 +5,7 @@ class estatePropertyTag(models.Model):
     _name = 'estate.property.tag'
 
     name = fields.Char("Name", required=True)
+
+    _sql_constraints = [
+        ('check_unique_tag_name', 'unique(name)', 'Name of Tag already found')
+    ]
