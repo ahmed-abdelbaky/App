84f3da977114dcd809d534618e679f61b32ce75c
Ahmed <ahmed.abdelbakey.18@gmail.com>
2023-01-10 19:18:16 +0300

    [ADD] add sql constrain on type name

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -5,3 +5,7 @@ class estatePropertyType(models.Model):
     _name = "estate.property.type"
 
     name = fields.Char('Name', required=True)
+
+    _sql_constraints = [
+        ('check_unique_type_name', 'unique(name)', 'Name of type already found')
+    ]
