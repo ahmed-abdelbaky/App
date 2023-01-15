220d2a99e2888029ddbd1063ddcc2aaec228c53a
Ahmed <ahmed.abdelbakey.18@gmail.com>
2023-01-15 12:20:35 +0300

    [ADD] add one2many field

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -5,6 +5,7 @@ class estatePropertyType(models.Model):
     _name = "estate.property.type"
 
     name = fields.Char('Name', required=True)
+    property_ids = fields.One2many('estate.property', 'type_id')
 
     _sql_constraints = [
         ('check_unique_type_name', 'unique(name)', 'Name of type already found')
