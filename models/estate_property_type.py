e429f94984fd6020b2dc0de9f574ceda71f5ddc9
Ahmed <ahmed.abdelbakey.18@gmail.com>
2023-01-16 00:03:02 +0300

    [ADD] add order by name

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -3,6 +3,7 @@ from odoo import models, fields, api
 
 class estatePropertyType(models.Model):
     _name = "estate.property.type"
+    _order = "name"
 
     name = fields.Char('Name', required=True)
     property_ids = fields.One2many('estate.property', 'type_id')
