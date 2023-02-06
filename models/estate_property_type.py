bcaa0dacadf0dceb25a887ce65412f544fb8d9aa
Ahmed <ahmed.abdelbakey.18@gmail.com>
2023-02-06 11:01:57 +0300

    [ADD] add new Many2one field

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -8,7 +8,16 @@ class estatePropertyType(models.Model):
     name = fields.Char('Name', required=True)
     property_ids = fields.One2many('estate.property', 'type_id')
     sequence = fields.Integer('Sequence')
-
+    offer_ids = fields.One2many('estate.property.offer', 'property_type_id')
+    offer_count = fields.Integer('offer count', compute='_compute_count_offer')
     _sql_constraints = [
         ('check_unique_type_name', 'unique(name)', 'Name of type already found')
     ]
+
+    @api.depends('offer_ids')
+    def _compute_count_offer(self):
+        for record in self:
+            if record.offer_ids:
+                record.offer_count = len(record.offer_ids)
+            else:
+                record.offer_count = 0
